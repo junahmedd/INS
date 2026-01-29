4f4faf3f53765bfcd91918bc45a249e35db804c2
Junaid Ahmed <mehjunaidd@gmail.com>
2026-01-29 17:11:41 +0530

    CeaserCipher.java

--- CeaserCipher.java
+++ CeaserCipher.java
@@ -1,45 +1,48 @@
 import java.util.Scanner;
-public class CeaserCipher {
-  public static  void mian(String [] args) {
-    Scanner sc = new Scanner(System.in);
-    
-    System.out.println("Enter text: ");
-    String text = sc.nextLine();
-    
-    System.out.println("Enter key value: ");
-    int key = sc.nextInt();
+public class CaesarCipher {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Encrypted Text: "+ encrypt(text,key));
-    System.out.println("Decrypted Text: "+decrypt(text,key));
-  }   
-static String encrypt(String text,int key){
-  String encryptedText = " ";
-  key = key % 26;
-  for(int i = 0 ; i < text.length() ; i++){
-    char ch = text.CharAt(i);
-    if( ch >= 'A' && ch <= 'Z'){
-      ch = (char)((ch - 'A' + key + 26) % 26 + 'A');
+        System.out.print("Enter the text to encrypt: ");
+        String text = scanner.nextLine();
+
+        System.out.print("Enter the key value: ");
+        int key = scanner.nextInt();
+
+        String encrypted = encrypt(text, key);
+        System.out.println("Encrypted text: " + encrypted);
+
+        String decrypted = decrypt(encrypted, key);
+        System.out.println("Decrypted text: " + decrypted);
+
+        scanner.close();
     }
-    else if ( ch >= 'a' && ch <= 'z'){
-      ch = (char)((ch - 'a' + key + 26) % 26 + 'a');
+    public static String encrypt(String text, int key) {
+        String encryptedText = "";
+        key = key % 26; 
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                ch = (char) ((ch - 'a' + key + 26) % 26 + 'a');
+            } else if (ch >= 'A' && ch <= 'Z') {
+                ch = (char) ((ch - 'A' + key + 26) % 26 + 'A');
+            }
+            encryptedText += ch;
+        }
+        return encryptedText;
     }
-    encryptedText += ch;
-  }
-  return encryptedText;
-}
-  public static String decrypt(String ciphertext, int key){
-    String decryptedText = " ";
-    key = key % 26;
-    for(int i = 0; i < ciphertext.length(); i++){
-      char ch = ciphertext.charAt(i);
-    if( ch >= 'A' && ch <= 'Z'){
-      ch = (char)((ch - 'A' - key + 26) % 26 + 'A');
+    public static String decrypt(String text, int key) {
+        String decryptedText = "";
+        key = key % 26;
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (ch >= 'a' && ch <= 'z') {
+                ch = (char) ((ch - 'a' - key + 26) % 26 + 'a');
+            } else if (ch >= 'A' && ch <= 'Z') {
+                ch = (char) ((ch - 'A' - key + 26) % 26 + 'A');
+            }
+            decryptedText += ch;
+        }
+        return decryptedText;
     }
-    else if ( ch >= 'a' && ch <= 'z'){
-      ch = (char)((ch - 'a' - key + 26) % 26 + 'a');
-    }
-    decryptedText += ch;
-    }
-    return decryptedText;
-  }
 }
